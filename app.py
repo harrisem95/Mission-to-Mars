dbf0adbe0b414e9be51af1f5a368bd168a6fc783
Emily Harris <emily@Chriss-Air.lan>
2022-10-12 19:09:53 -0400

    git push

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import scraping
 app = Flask(__name__)
 
 #Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27-17/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -18,7 +18,7 @@ def index():
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update_one({}, {"set":mars_data}, upsert=True)
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
     return redirect('/', code=302)
 
 if __name__ == "__main__":
